8db5af20f908952e66d3473757153ef2d6e6e366
Miguel Liebana <mi.liebana@gmail.com>
2015-03-14 11:19:38 +0000

    Add producer class which can send messages easily to the broker

---
+++ lib/Producer.php
@@ -0,0 +1,137 @@
+<?php
+namespace znk3r\MQlib;
+use PhpAmqpLib\Connection\AbstractConnection;
+use znk3r\MQlib\Broker\AbstractBroker;
+use znk3r\MQlib\Broker\RabbitMQ;
+use znk3r\MQlib\Exchange\AbstractExchange;
+use znk3r\MQlib\Message\Outgoing;
+
+/**
+ * Class Producer.
+ *
+ * @version   1.0.0
+ *
+ * @author    Miguel Angel Liebana <mi.liebana@gmail.com>
+ * @copyright 2015 Miguel Angel Liebana
+ */
+class Producer
+{
+    /** @var AbstractBroker $broker Broker configuration class */
+    protected $broker;
+
+    /** @var AbstractExchange $exchange */
+    protected $exchange;
+
+    /**
+     * Create a new producer and defining its connection with a broker
+     *
+     * By default, the producer will try to use a RabbitMQ broker with the default options. Those defaults
+     * can be changed sending an array of options.
+     *
+     * @param AbstractBroker|array $broker
+     * @throws Exception if the constructor isn't able to create a broker object
+     */
+    public function __construct($broker = array())
+    {
+        if ($broker instanceof AbstractBroker) {
+            $this->broker = $broker;
+        } elseif (is_array($broker)) {
+            $this->broker = new RabbitMQ($broker);
+        } else {
+            throw new Exception('The producer needs a valid broker or a config array.');
+        }
+    }
+
+    /**
+     * @return AbstractBroker
+     */
+    public function getBroker()
+    {
+        return $this->broker;
+    }
+
+    /**
+     * Opens the connection with the broker
+     *
+     * You can specify the type of connection to create, or use the default one (StreamIO)
+     * The method also accepts an optional AbstractConnection object to use, instead of creating a new one.
+     *
+     * @param string|null $type Connection type (see Broker\Connection\Factory)
+     * @param AbstractConnection|null $connection
+     *
+     * @return $this
+     */
+    public function connect($type = null, $connection = null)
+    {
+        if (null !== $type) {
+            $this->broker->setConnectionType($type);
+        }
+
+        $this->broker->connect($connection);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function disconnect()
+    {
+        $this->broker->disconnect();
+
+        return $this;
+    }
+
+    /**
+     * Open a channel
+     *
+     * You can send a channel id as parameter, or null to pick a random one, or use the default option from
+     * the broker with false.
+     *
+     * @param int|null|bool $channelId
+     *
+     * @return $this
+     */
+    public function openChannel($channelId = false)
+    {
+        $this->broker->openChannel($channelId);
+
+        return $this;
+    }
+
+    /**
+     * @param AbstractExchange $exchange
+     * @return $this
+     */
+    public function sendTo(AbstractExchange $exchange)
+    {
+        $this->exchange = $exchange;
+        $this->getBroker()->getChannel()->declareExchange($exchange);
+
+        return $this;
+    }
+
+    /**
+     * @return AbstractExchange
+     */
+    public function getExchange()
+    {
+        return $this->exchange;
+    }
+
+    /**
+     * @param Outgoing $message
+     * @param AbstractExchange|null $exchange
+     * @return $this
+     */
+    public function publish(Outgoing $message, $exchange = null)
+    {
+        if (null !== $exchange) {
+            $this->sendTo($exchange);
+        }
+
+        $this->getBroker()->getChannel()->sendMessage($message, $this->getExchange());
+
+        return $this;
+    }
+}
